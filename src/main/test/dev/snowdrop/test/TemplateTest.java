c8a7223adc748d505da5cdad399cbef40a2dac99
cmoulliard <cmoulliard@redhat.com>
2025-05-16 18:20:34 +0200

    Add qute and test case
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ src/main/test/dev/snowdrop/test/TemplateTest.java
@@ -0,0 +1,22 @@
+package dev.snowdrop.test;
+
+import io.quarkus.qute.Engine;
+import io.quarkus.qute.Template;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@QuarkusTest
+public class TemplateTest {
+    @Test
+    public void test() throws IOException {
+        InputStream is = Test.class.getResourceAsStream("/kubeadm-from-1.24.yaml");
+        String kubeAdmin = new String(is.readAllBytes());
+        Engine engine = Engine.builder().addDefaults().build();
+        Template kubeAdminTmpl = engine.parse(kubeAdmin);
+        System.out.println("Result: "  + kubeAdminTmpl.data("nodeName","my-kind").render());
+    }
+
+}
