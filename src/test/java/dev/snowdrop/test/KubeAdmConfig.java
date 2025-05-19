aa87e7ad4fe8d9f7a182b85402337558f22efa4d
cmoulliard <cmoulliard@redhat.com>
2025-05-19 14:37:30 +0200

    Add movie example
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ src/test/java/dev/snowdrop/test/KubeAdmConfig.java
@@ -0,0 +1,31 @@
+package dev.snowdrop.test;
+
+public class KubeAdmConfig {
+    private String nodeName;
+    private String kubernetesVersion;
+    private String nodeIp;
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getKubernetesVersion() {
+        return kubernetesVersion;
+    }
+
+    public void setKubernetesVersion(String kubernetesVersion) {
+        this.kubernetesVersion = kubernetesVersion;
+    }
+
+    public String getNodeIp() {
+        return nodeIp;
+    }
+
+    public void setNodeIp(String nodeIp) {
+        this.nodeIp = nodeIp;
+    }
+}
